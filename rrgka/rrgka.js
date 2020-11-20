c3503a07fc3f5a597fd2114b1e0a998b3c588c64
jtschuler <jtschuler007@yahoo.com>
2020-11-20 01:08:25 -0500

    update rrgka

--- rrgka/rrgka.js
+++ rrgka/rrgka.js
@@ -1,5 +1,19 @@
 const Discord = require('discord.js');
 const auth = require('./authorize')
+const commandprefix = 'r-'
 
 
 const client = auth.authorize();
+
+client.on('ready', () => {
+	console.log("Connected as " + client.user.tag);
+})
+
+client.on('message', msg => {
+	if (msg.content.substring(0,2) == commandprefix) {
+		command = msg.content.substring(2);
+		if (command === 'ping') {
+			msg.reply('pong');
+		}
+	}
+});
