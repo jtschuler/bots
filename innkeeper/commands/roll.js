d79682e15eb8b56922f5eef1912bafb390115454
Jadon T Schuler <jtschuler007@yahoo.com>
2020-11-21 19:14:05 -0500

    Added random rolling functionality

--- innkeeper/commands/roll.js
+++ innkeeper/commands/roll.js
@@ -2,13 +2,13 @@ module.exports = {
 	name: 'roll',
 	description: 'Rolls the dice in the arguments',
 	args: true,
-	usage: 'roll <#d#>',
+	usage: '-i roll <#d#>',
 	guildOnly: true,
 	execute(message, args) {
 		// Test reg exp for proepr formatting
-		var diceRE = /^[1-9][0-9]*d[1-9][0-9]*$/;
+		var diceRE = /^[1-9]\d*d[1-9]\d*$/;
 		if (!diceRE.test(args[0]))
-			return message.channel.send(`Improper formatting.`);
+			return message.channel.send(`Improper formatting.\nUsage: ${this.usage}`);
 		
 		dice = args[0].split('d');
 		numDice = dice[0];
@@ -20,7 +20,30 @@ module.exports = {
 		if (diceType > 100)
 			return message.reply('what kinda weird dice are you tring to roll, mate?')
 		
-		// TODO
-		message.channel.send(`You wanted to roll ${numDice} ${diceType}-sided dice!`)
+		
+		// Generate dice rolls
+		var array = generateValues(numDice, diceType);
+		const rolls = array[0];
+		const total = array[1];
+		
+		var toPrint = 'Rolls:\n';
+		for(i = 0; i < rolls.length; i ++) {
+			toPrint += rolls[i] + ', ';
+		}
+		toPrint = toPrint.slice(0, toPrint.length - 2) + `\n\nTotal: ${total}`;
+		
+		message.channel.send(toPrint)
 	},
 };
+
+function generateValues(rolls, maxValue) {
+	var array = [];
+	total = 0
+	for(i = 0; i < rolls; i++) {
+		roll = Math.floor(Math.random() * maxValue + 1);
+		total += roll;
+		array.push(roll);
+	}
+	
+	return [array, total];
+}
